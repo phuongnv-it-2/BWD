7b66fc590b4a67e19913a71e99178966ce255a72
phuongnv-it-2 <nvphuongaz@gmail.com>
2025-05-08 13:25:23 +0700

    change some texts

--- script.js
+++ script.js
@@ -4,3 +4,16 @@
         menubar.classList.toggle("active");
       document.querySelector(".menu-items").classList.toggle("active");
     });
+      const topMenu = document.querySelector(".top");
+    let lastScrollY = window.scrollY;
+    
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > lastScrollY) {
+            // Cuộn xuống
+            topMenu.style.transform = "translateY(-100%)"; // Ẩn thanh menu
+        } else {
+            // Cuộn lên
+            topMenu.style.transform = "translateY(0)"; // Hiển thị thanh menu
+        }
+        lastScrollY = window.scrollY;
+    });
