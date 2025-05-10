2301ae5f62018a0d0a81a8abbe33c6405971326b
phuongnv-it-2 <nvphuongaz@gmail.com>
2025-05-10 19:49:45 +0700

    tuan dau buoi

--- JS/HomeScript.js
+++ JS/HomeScript.js
@@ -17,3 +17,28 @@
         }
         lastScrollY = window.scrollY;
     });
+
+
+  /*pageanimation-------------------------------------
+  ------------------------------------------------
+  ------------------------------------------------------*/
+  document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        },
+        {
+            threshold: 0.1, 
+        }
+    );
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
