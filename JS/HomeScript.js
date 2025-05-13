254908bb857c482aa7e0c3d76bae42bbe00b0738
phuongnv-it-2 <nvphuongaz@gmail.com>
2025-05-13 17:15:05 +0700

    upDateHome

--- JS/HomeScript.js
+++ JS/HomeScript.js
@@ -1,24 +1,4 @@
 
-    const menubar = document.querySelector(".menu-bar");
-    menubar.addEventListener("click", function () {
-        menubar.classList.toggle("active");
-      document.querySelector(".menu-items").classList.toggle("active");
-    });
-      const topMenu = document.querySelector(".top");
-    let lastScrollY = window.scrollY;
-    
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > lastScrollY) {
-            // Cuộn xuống
-            topMenu.style.transform = "translateY(-100%)"; // Ẩn thanh menu
-        } else {
-            // Cuộn lên
-            topMenu.style.transform = "translateY(0)"; // Hiển thị thanh menu
-        }
-        lastScrollY = window.scrollY;
-    });
-
-
   /*pageanimation-------------------------------------
   ------------------------------------------------
   ------------------------------------------------------*/
